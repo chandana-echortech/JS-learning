e7e7b90cdcd1947a4f899f931f0395c80a4805de
echortech <147915250+echortech@users.noreply.github.com>
2025-04-07 11:57:35 +0530

    your message

--- run.js
+++ run.js
@@ -1,5 +1,12 @@
 // problem 2
-let n = 5; 
+let n = 5;                                
+for (let i = n; i >= 1; i--) {
+  let row = '';
+  for (let j = 1; j <= i; j++) {
+    row += '*';
+  }
+  console.log(row);
+}
 //code here
 // use only for loops. dont use .repeat() or .trim()
 
@@ -10,3 +17,4 @@ let n = 5;
 // * * *
 // * *
 // *
+
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //right traingle
-let n = 10; // you can change this to 10 or any number
-for (let i = 1; i <= n; i++) {
-  let row = '';
-  for (let j = 1; j <= i; j++) {
-    row += '*';
-  }
-  console.log(row);
-}
+// let n = 10; // you can change this to 10 or any number
+// for (let i = 1; i <= n; i++) {
+//   let row = '';
+//   for (let j = 1; j <= i; j++) {
+//     row += '*';
+//   }
+//   console.log(row);
+// }
 
-//left traingle
+//inverted traingle
 // let n = 10;                                
 // for (let i = n; i >= 1; i--) {
 //   let row = '';
